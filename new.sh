252a9454fcfc3dae41e7b9702b2b31d8b36f671e
stevensia <beacon1024@gmail.com>
2016-01-19 20:53:32 +0800

    Birthday

---
+++ new.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]
+ then echo "Usage: $0 title category"
+ exit
+fi
+TITLE=$1
+TISP=`echo $TITLE | sed 's/ /-/g'`
+DATE=`date "+%Y-%m-%d %H:%M:%S"`
+FDATE=`date "+%Y-%m-%d"`
+CATE=$2 
+
+FILE="$FDATE-$TISP.md"
+
+echo "---
+layout: post
+title:  "$TITLE"
+date:   "$DATE"
+categories: "$CATE"
+tags: "CATE"
+excerpt: 
+---
+" > $FILE
+
+echo $FILE
--- restart.sh
+++ restart.sh
@@ -2,4 +2,13 @@ ps -fe|grep jekyll|grep -v grep |awk '{print $2}'|xargs -i kill -9 {}
 echo "Killed jekyll.."
 bundle exec jekyll serve &
 echo Finished
+sleep 10
+ps -fe|grep jekyll|grep -v grep |awk '{print $2}'|xargs -i kill -9 {}
+exit 0
 
+gem install bundler
+bundle install
+
+#Gemfile
+source 'https://ruby.taobao.org'
+gem 'github-pages'
